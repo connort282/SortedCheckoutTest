5d87df755e72702aa01ad8d1e15106bd082bcb3a
Connor Taylor <Connor.Taylor@parcelhub.co.uk>
2020-11-29 15:35:32 +0000

    Adding private field to hold items passed in through ItemLookup constructor

--- SortedCheckoutTest.Logic/ItemLookup.cs
+++ SortedCheckoutTest.Logic/ItemLookup.cs
@@ -7,9 +7,30 @@ namespace SortedCheckoutTest.Logic
 {
     public class ItemLookup : IItemLookup
     {
+        private IDictionary<string, Item> items;
+
+        public ItemLookup(IDictionary<string, Item> Items)
+        {
+            items = Items;
+        }
+
+        /// <summary>
+        /// Retrives the item object if it exists
+        /// </summary>
+        /// <param name="SKU">SKU of the item</param>
+        /// <returns>
+        /// Returns null if item does not exist, returns Item if it does exist.
+        /// Throws ArgumentException if SKU if null or whitespace
+        /// </returns>
         public Item GetItem(string SKU)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(SKU))
+            {
+                throw new ArgumentException($"'{nameof(SKU)}' cannot be null or whitespace", nameof(SKU));
+            }
+
+            items.TryGetValue(SKU, out Item item);
+            return item;
         }
     }
 }
