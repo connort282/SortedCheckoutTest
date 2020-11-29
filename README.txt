8153f4b8942466be9fa70c0a24a58369e96a879b
Connor Taylor <Connor.Taylor@parcelhub.co.uk>
2020-11-29 16:06:12 +0000

    Implementing console interaction to run through some basic testing

--- README.txt
+++ README.txt
@@ -5,13 +5,13 @@ Checklist to follow:
 Create a new solution
 	Include a class library for the logic | Added
 	Include a test library for unit tests (feel free to use whatever test library you are most comfortable with) | Added
-Prove you can scan an item at a checkout
+Prove you can scan an item at a checkout | done
 	Adding my own points:
 		Need a class to hold the item information | Item.cs
 		Need a class that can validate and return information about an item | IItemLookup and the concrete ItemLookup
 		State is going to be handled in memory by just maintaining an object
 		Need a class that performs the actual logic | Checkout.cs
-Prove you can request the total price
+Prove you can request the total price | done
 Introduce special offers
 	Amend your prior implementation to consider offers on items
 Prove you can request the total price inclusive of offers
--- SortedCheckoutTest/Program.cs
+++ SortedCheckoutTest/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SortedCheckoutTest.Logic;
+using SortedCheckoutTest.Logic.Models;
+using System;
+using System.Collections.Generic;
 
 namespace SortedCheckoutTest
 {
@@ -6,7 +9,37 @@ namespace SortedCheckoutTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Item> itemDictionary = new Dictionary<string, Item>()
+            {
+                {"A99", new Item(){SKU = "A99",Price = 0.50m } },
+                {"B15", new Item(){SKU = "B15",Price = 0.30m } },
+                {"C40", new Item(){SKU = "C40",Price = 0.60m } },
+            };
+
+            IItemLookup itemLookup = new ItemLookup(itemDictionary);
+            ICheckout checkout = new Checkout(itemLookup);
+
+            Console.WriteLine("Please scan your items or ** to calculate price");
+
+            while (true)
+            {
+                Console.Write("Please enter selection: ");
+                var enteredText = Console.ReadLine();
+
+                switch (enteredText)
+                {
+                    case "**":
+                        Console.WriteLine($"The current total is: {checkout.GetTotalPrice()}");
+                        break;
+                    default:
+                        if (checkout.AddItemToBasket(enteredText) == false)
+                        {
+                            Console.WriteLine("Unable to find SKU");
+                        }
+                        break;
+                }
+
+            }
         }
     }
 }
